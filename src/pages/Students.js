644f1d04a63d833bdc8630ad58b8b393142e3883
Ajith <ajith.pgm89@gmail.com>
2023-01-28 23:42:57 +0530

    bug fix - rendering student table instead of body

--- src/pages/Students.js
+++ src/pages/Students.js
@@ -1,12 +1,12 @@
 import MainNavigation from "../components/Layout/MainNavigation";
-import StudentTable from "../components/students/StudentBody";
+import StudentBody from "../components/students/StudentBody";
 
 export default function Students() {
   return (
     <>
       <MainNavigation page="students" />
       <div className="d-flex flex-column align-items-center p-2">
-        <StudentTable />
+        <StudentBody />
       </div>
     </>
   );
